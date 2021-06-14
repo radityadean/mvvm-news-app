87913f1cc217a7f51963710c19852ec5b882db42
raditya dean <radityadean91@gmail.com>
2021-06-14 10:48:17 +0700

    update mvvm news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constans.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constans.kt
@@ -4,6 +4,7 @@ class Constans {
 
     companion object {
         const val API_KEY = "631f7d6e68ad4866bc66ff87304967d8"
-        const val  BASE_URL = "https://newsapi.org"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
